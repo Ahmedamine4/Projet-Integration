0e503f04c019203e20a399c4f50a63b86007324c
Marouanelmzr <elmozariahimarouane05@gmail.com>
2026-04-16 00:33:00 +0100

    Add: design system et fonts

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,7 +5,10 @@
 
 <template>
     <div>
-        <h1>Portfolio Builder</h1>
+        <div class="home_section_1">
+            <h1 class="title_brand">Your <span class="bold">Portfolio,</span></h1>
+            <h1 class="title_brand">Certified for the Real World</h1>
+        </div>
         <p>Create and manage your portfolio.</p>
         <button @click="router.push('/register')">Get Started</button>
     </div>
