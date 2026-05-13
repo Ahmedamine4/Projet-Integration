0457e6a28729fc9892ba5e56eaced81398378566
El Ghazouani Reda <elghazouani.reda05@gmail.com>
2026-05-13 09:54:44 +0100

    push de route / controller / service de certification

---
+++ backend/src/controllers/certification.controller.js
@@ -0,0 +1,45 @@
+//Done
+
+import { body } from 'express-validator';
+import * as certificationService from '../services/certification.service.js';
+import asyncHandler from '../utils/asyncHandler.js';
+
+export const addCertification = asyncHandler(async (req, res) => {
+  const { 
+    title, 
+    issuingOrganization, 
+    issueDate, 
+    credentialUrl, 
+    description, 
+    code 
+  } = req.body;
+
+  const userId =  req.user.utilisateur_id ;
+
+  const result = await certificationService.createCertification({
+    title,
+    issuingOrganization,
+    issueDate,
+    credentialUrl,
+    description,
+    code
+  }, userId);
+
+  res.status(201).json({
+    success: true,
+    message: "Certification ajoutée avec succès",
+    data: result
+  });
+});
+
+export const getMyCertifications = asyncHandler(async (req, res) => {
+  const userId = req.user.etudiant_utilisateur_id || req.user.utilisateur_id || req.user.id;
+
+  const certifications = await certificationService.getMyCertifications(userId);
+
+  res.json({
+    success: true,
+    count: certifications.length,
+    data: certifications
+  });
+});
---
+++ backend/src/routes/certification.route.js
@@ -0,0 +1,27 @@
+//done
+import express from 'express';
+import { authMiddleware, authorizeRoles } from '../middleware/auth.middleware.js';
+import { ROLES } from '../middleware/auth.middleware.js';
+import {
+  addCertification,
+  getMyCertifications,
+} from '../controllers/certification.controller.js';
+
+const router = express.Router();
+
+router.use(authMiddleware);
+
+router.post(
+  '/',
+  authorizeRoles(ROLES.ETUDIANT),
+  //validateAddCertification,
+  addCertification
+);
+
+router.get(
+  '/me',
+  authorizeRoles(ROLES.ETUDIANT),
+  getMyCertifications
+);
+
+export default router;
