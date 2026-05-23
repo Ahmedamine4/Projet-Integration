be1c70551c5ec0cdd5b557438783a76b357bcfa1
El Ghazouani Reda <elghazouani.reda05@gmail.com>
2026-05-23 11:21:35 +0100

    get all users fix

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -11,11 +11,13 @@ import {
   authMiddleware,
   authorizeRoles,
 } from '../middlewares/auth.middleware.js';
+import { getAllUsers } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 router.use(authMiddleware, authorizeRoles(ROLES.ADMIN));
 router.post('/assigner-directeur', ajoutDirecteur);
 router.get('/professionnels/en-attente', listProfessionnelsEnAttente);
+router.get('/users',getAllUsers);
 router.patch('/professionnels/:id/valider', validerProfessionnel);
 router.patch('/professionnels/:id/refuser', refuserProfessionnel);
 router.patch('/utilisateurs/:id/bloquer', bloquerUtilisateurController);
